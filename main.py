8c72a99d2d7cadb3bcbeb87a5719ed340f0977ba
Jhomar1158-ux <jhomarcristianelias@gmail.com>
2022-06-08 22:17:55 -0500

    dinamic number

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+import time
 
+# Ubicación del chromedrive local para aplicaciones de Selenium
 service = Service('D:\\chromeDriver\\chromedriver.exe')
 
 def get_driver():
@@ -18,14 +20,23 @@ def get_driver():
 
     options.add_argument("disable-blink-features=AutomationControlled")
     driver = webdriver.Chrome(service=service, options=options)
-    # Recibimos la URL
+    # Hacemos la solicitud a la siguiente URL
     driver.get("https://automated.pythonanywhere.com/")
     return driver
 
+def clean_text(text):
+    # Nos quedamos solo con el número usando split() para recortarlo 
+    output = float(text.split(": ")[1])
+    return output
+
 def main():
     driver = get_driver()
-    # x_path -> 
-    element = driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[1]")
-    return element.text
+    # El número que queremos aparece luego de 2 seg en el navegador por lo que debemos usar sleep para
+    # esperar a que aparezca y capturarlo
+    time.sleep(3)
+    # Accedemos a la etiqueta que contiene nuestro texto
+    element = driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[2]")
+    # Convertimos a TEXTO usando la propiedad text
+    return clean_text(element.text)
 
 print(main())
